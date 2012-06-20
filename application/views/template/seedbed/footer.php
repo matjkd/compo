d1c4a5b2b68d79efe27025353c84c95375c8ed0b
Mat Sadler <mat@redstudio.co.uk>
2012-06-20 12:21:23 +0000

    changes brought up to date with latest from seedbed

--- application/views/template/seedbed/footer.php
+++ application/views/template/seedbed/footer.php
@@ -14,9 +14,9 @@ $(document).ready(function () {
 	</script>
   
    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.js"></script>
-<!-- <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=true&amp;key=<?=$maps_api?>" type="text/javascript"></script>-->
-
-  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script> 
+  <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places"></script>
+ 
 
   <!-- scripts concatenated and minified via ant build script-->
    <script src="http://cdn.jquerytools.org/1.2.6/all/jquery.tools.min.js"></script>
@@ -25,6 +25,14 @@ $(document).ready(function () {
      <script src="<?=base_url()?>js/datatables.js"></script>
       <script src="<?=base_url()?>js/script.js"></script>
 
+      
+      <script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
   <!-- end concatenated and minified scripts-->
   
   
